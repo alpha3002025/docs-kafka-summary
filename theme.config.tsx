e4c3fef56a91bba760d3c74ad64d6a2fe84171c4
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-08 12:24:34 +0900

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Kafka Summary</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-kafka-summary',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-kafka-summary',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
